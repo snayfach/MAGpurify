fe3d3c67940d282ef9eb4f0b6ecfbafd0f21dde3
Antonio Camargo <antoniop.camargo@gmail.com>
2020-03-04 16:22:01 -0800

    Fix scikit-learn dependency and bump version to 2.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="magpurify",
-    version="2.1.0",
+    version="2.1.1",
     packages=find_packages(),
     license="GNU General Public License v3.0",
     description="Identify and remove incorrectly binned contigs from metagenome-assembled genomes.",
@@ -31,7 +31,7 @@ setup(
         "biopython",
         "numpy",
         "pandas",
-        "sklearn",
+        "scikit-learn",
     ],
     python_requires=">=3.6",
     entry_points={"console_scripts": ["magpurify=magpurify.cli:cli"]},
